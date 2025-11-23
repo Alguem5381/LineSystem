a3f30b8bd2ff203498f4ba018828193a8ad9e2a3
Alguem5381 <alguem5381@gmail.com>
2025-11-22 22:18:15 -0400

    Correção da assinatura da função da page

---
+++ pages/search_page.h
@@ -0,0 +1,9 @@
+#ifndef SEARCH_PAGE_H
+#define SEARCH_PAGE_H
+
+#include <page_results.h>
+#include <style.h>
+
+PageResult init_search_page(Style const *style, wchar_t const *search_text, wchar_t const *label_text, wchar_t const *elements[], int const elements_length);
+
+#endif //SEARCH_PAGE_H
