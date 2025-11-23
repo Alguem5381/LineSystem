31b7d720300aa92a3b2b61897a8b5f00c52281f4
Alguem5381 <alguem5381@gmail.com>
2025-11-23 16:42:48 -0400

    Correção de assinatura na função page e mudança da capitura de teclas

--- pages/search_page.c
+++ pages/search_page.c
@@ -56,79 +56,93 @@ PageResult init_search_page(Style const *style, wchar_t const *search_text, wcha
     int need_draw = 1;
     int running = 1;
     int selected = 0;
-    int character = KEY_RESIZE;
+
+    //Para capituras de teclas
+    int status = 256;
+    wint_t character = KEY_RESIZE;
 
     while(running)
     {
-        switch (character)
+        if (status) //Teclas especias vão aqui
         {
-        case KEY_RESIZE:
-        {
-            resize_term(0, 0);
-            general_context.endx = COLS;
-            general_context.endy = LINES;
+            switch (character)
+            {
+            case KEY_RESIZE:
+            {
+                resize_term(0, 0);
+                general_context.endx = COLS;
+                general_context.endy = LINES;
 
-            DrawContext main_panel_context = general_context;
-            DrawContext left_panel_context;
-            DrawContext right_panel_context;
+                DrawContext main_panel_context = general_context;
+                DrawContext left_panel_context;
+                DrawContext right_panel_context;
 
-            main_panel_context.starty += 5;
-            main_panel_context.endy -= 5;
+                main_panel_context.starty += 5;
+                main_panel_context.endy -= 5;
 
-            split_context(&left_panel_context, &main_panel_context, 50, 0, 0);
-            split_context(&right_panel_context, &main_panel_context, 50, 1, 0);
+                split_context(&left_panel_context, &main_panel_context, 50, 0, 0);
+                split_context(&right_panel_context, &main_panel_context, 50, 1, 0);
 
-            split_context(&list_context, &left_panel_context, 20, 1, 1);
-            split_context(&search_bar_context, &left_panel_context, 30, 0, 1);
-            split_context(&time_bar_context, &right_panel_context, 30, 0, 1);
-            split_context(&label_context, &right_panel_context, 20, 1, 1);
+                split_context(&list_context, &left_panel_context, 20, 1, 1);
+                split_context(&search_bar_context, &left_panel_context, 30, 0, 1);
+                split_context(&time_bar_context, &right_panel_context, 30, 0, 1);
+                split_context(&label_context, &right_panel_context, 20, 1, 1);
 
-            need_draw = 1;
+                need_draw = 1;
 
-            break;
+                break;
+            }
+
+            case KEY_UP:
+                if (list_context.element_in_focus > 0)
+                {
+                    list_context.element_in_focus--;
+                    need_draw = 1;
+                }
+                break;
+
+            case KEY_DOWN:
+                if (list_context.element_in_focus < elements_length - 1)
+                {
+                    list_context.element_in_focus++;
+                    need_draw = 1;
+                }
+                break;
+
+            default:
+                break;
+            }
         }
-
-        case KEY_UP:
-            if (list_context.element_in_focus > 0)
+        else        //Teclas comuns
+        {
+            switch (character)
             {
-                list_context.element_in_focus--;
-                need_draw = 1;
+            case 27:
+                result.action = page_action_back;
+                running = 0;
+                break;
+            
+            default:
+                break;
             }
-            break;
-
-        case KEY_DOWN:
-            if (list_context.element_in_focus < elements_length - 1)
-            {
-                list_context.element_in_focus++;
-                need_draw = 1;
-            }
-            break;
-
-        case 27:
-            result.action = page_action_back;
-            running = 0;
-            break;
-
-        default:
-            break;
         }
 
         if (need_draw)
-        {
-            int sucessful = 1;
-            sucessful *= draw_base_page(L"Buscar rotas - origem", &general_context);
-            sucessful *= draw_footer(keys, options, keys_length, &general_context);
-            sucessful *= draw_text_box(search_text, &search_bar_context);
-            sucessful *= draw_text_box(time_text, &time_bar_context);
-            sucessful *= draw_label(label_text, &label_context);
-            sucessful *= draw_list(elements, elements_length, &list_context);
+            {
+                int sucessful = 1;
+                sucessful *= draw_base_page(L"Buscar rotas - origem", &general_context);
+                sucessful *= draw_footer(keys, options, keys_length, &general_context);
+                sucessful *= draw_text_box(search_text, &search_bar_context);
+                sucessful *= draw_text_box(time_text, &time_bar_context);
+                sucessful *= draw_label(label_text, &label_context);
+                sucessful *= draw_list(elements, elements_length, &list_context);
 
-            need_draw = 0;
-            refresh();
-        }
+                need_draw = 0;
+                refresh();
+            }
 
         if(running)
-            character = getch();
+            status = get_wch(&character);
     }
 
     return result;
