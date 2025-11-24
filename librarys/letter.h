2cd900078e1df6f8ce4abe68bf4a1afbb95b5826
Alguem5381 <alguem5381@gmail.com>
2025-11-24 01:52:20 -0400

    Atualização no nome dos parametros das funções e adição do espaço como letra

--- librarys/letter.h
+++ librarys/letter.h
@@ -3,13 +3,13 @@
 
 #include <wchar.h>
 
-int is_letter(char const letter);
+int is_letter(char const character);
 
-int is_wletter(wchar_t const letter);
+int is_wletter(wchar_t const character);
 
-int is_number(char const letter);
+int is_number(char const character);
 
-int is_wnumber(wchar_t const letter);
+int is_wnumber(wchar_t const character);
 
 int remove_lastw(wchar_t string[]);
 
