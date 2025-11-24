998870703075944c6747857583fd4c3dbabc6399
Alguem5381 <alguem5381@gmail.com>
2025-11-23 21:08:33 -0400

    Adições em PageActions e mudança do tipo de string de page PageResult

--- librarys/page_results.h
+++ librarys/page_results.h
@@ -1,18 +1,22 @@
 #ifndef PAGERESULTS_H
 #define PAGERESULTS_H
 
+#include <wchar.h>
+
 typedef enum PageAction
 {
+    page_action_fail,
     page_action_back,
     page_action_select,
-    page_action_text
+    page_action_text,
+    page_action_text_and_selected
 } PageAction;
 
 typedef struct PageResult
 {
     PageAction action;
     int selected_index;
-    char text[256];
+    wchar_t text[256];
 } PageResult;
 
 
