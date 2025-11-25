2d56615d3e87cd7703dfba185d23bb0ed9182c52
Alguem5381 <alguem5381@gmail.com>
2025-11-24 21:32:12 -0400

    Atualização da page e handle principal para o novo modelo

--- pages/main_page.c
+++ pages/main_page.c
@@ -2,18 +2,16 @@
 #include <ncursesw/ncurses.h>
 #include <main_page.h>
 #include <draw.h>
+#include <letter.h>
+#include <keys.h>
 
-//Inicializador da página
-
-PageResult init_main_page(Style const *style, void **persistence)
+PageResult init_main_page(PageArgs args)
 {
     PageResult result = {0};
 
     //Contextos
     DrawContext general_context =
     {
-        .startx = 0,
-        .starty = 0,
         .width = 100,
         .height = 100
     };
@@ -25,8 +23,8 @@ PageResult init_main_page(Style const *style, void **persistence)
     };
 
     //Definição de estilo para os contextos
-    set_style(style, &general_context);
-    set_style(style, &list_context);
+    set_style(args.style, &general_context);
+    set_style(args.style, &list_context);
 
     //Vetores utilizados
     wchar_t *keys[] = {L"Esc", L"↑", L"↓", L"↵"};
@@ -37,88 +35,95 @@ PageResult init_main_page(Style const *style, void **persistence)
     int keys_length = sizeof(keys) / sizeof(keys[0]);
     int elements_length = sizeof(elements) / sizeof(elements[0]);
 
-    //Variaveis para o loop
-    int need_draw = 1;
-    int running = 1;
-    int selected = 0;
+    // Para capitura teclas
+    Key key = unknown;
+    wint_t character;
 
-    int status = 256;
-    wint_t character = KEY_RESIZE;
+    // Variaveis para o loop
+    int need_draw = 1;
+    int need_split = 1;
+    int running = 1;
+    int is_popup_on = args.throw_popup;
+
 
     while(running)
     {
-        if (status == KEY_CODE_YES) //Teclas especiais vão aqui
+        // Tratamento de teclas
+        switch (key)
         {
-            switch (character)
+        case resize:
+            resize_term(0, 0);
+            need_split = 1;
+            need_draw = 1;
+            break;
+
+        case esc:
+            result.action = page_action_back;
+            running = 0;
+            break;
+
+        case up:
+            if (list_context.element_in_focus > 0)
             {
-            case KEY_RESIZE:
-            {
-                resize_term(0, 0);
-                general_context.endx = COLS;
-                general_context.endy = LINES;
-
-                split_context(&list_context, &general_context, 50, FIRST, HORIZONTAL);
-
-                list_context.starty = general_context.starty + 4;
-                list_context.endy = general_context.endy - 4;
-
+                list_context.element_in_focus--;
                 need_draw = 1;
-
-                break;
             }
+            break;
 
-            case '\n':
-                result.action = page_action_select;
-                result.selected_index = list_context.element_in_focus;
-                running = 0;
-                break;
-
-            case KEY_UP:
-                if (list_context.element_in_focus > 0)
-                {
-                    list_context.element_in_focus--;
-                    need_draw = 1;
-                }
-                break;
-
-            case KEY_DOWN:
-                if (list_context.element_in_focus < elements_length - 1)
-                {
-                    list_context.element_in_focus++;
-                    need_draw = 1;
-                }
-                break;
-
-            default:
-                break;
-            }
-        }
-        else    //Aqui vão as teclas comuns
-        {
-            switch (character)
+        case down:
+            if (list_context.element_in_focus < elements_length - 1)
             {
-            case 27:
-                result.action = page_action_back;
-                running = 0;
-                break;
-            
-            default:
-                break;
+                list_context.element_in_focus++;
+                need_draw = 1;
             }
+            break;
+
+        case enter:
+            result.action = page_action_select;
+            result.selected_index = list_context.element_in_focus;
+            running = 0;
+            break;
+
+        default:
+            break;
         }
 
+        // Definição das coordenadas dos contextos
+        if (need_split)
+        {
+            general_context.startx = 0;
+            general_context.starty = 0;
+            general_context.endx = COLS;
+            general_context.endy = LINES;
+
+            split_context(&list_context, &general_context, 50, FIRST, HORIZONTAL);
+
+            list_context.starty = general_context.starty + 4;
+            list_context.endy = general_context.endy - 4;
+
+            need_draw = 1;
+
+            need_split = 0;
+        }
+
+        // Desenho
         if (need_draw)
         {
             int sucessful = 1;
             sucessful *= draw_base_page(L"Menu principal", &general_context);
             sucessful *= draw_footer(keys, options, keys_length, &general_context);
             sucessful *= draw_list(elements, elements_length, &list_context);
+
             need_draw = 0;
             refresh();
         }
 
-        if(running)
-            status = get_wch(&character);
+        // Capitura de tecla
+        if (running)
+        {
+            int status = get_wch(&character);
+            key = get_key(character, status);
+        }
     }
 
     return result;
