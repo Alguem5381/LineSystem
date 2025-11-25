c21fd164c2875c9c051242c0eeb9cd2f54d63fa7
Alguem5381 <alguem5381@gmail.com>
2025-11-24 23:08:30 -0400

    Implementação da função para validar horários em strings

--- librarys/hours.c
+++ librarys/hours.c
@@ -3,7 +3,20 @@
 
 int is_valid_time(const wchar_t *string)
 {
-    
+    int length = wcslen(string);
+
+    //"19h30m" possui tamanho 6
+    if (length != 6) return 0;
+
+    if (is_number(string[0]) &&
+        is_number(string[1]) &&
+        (string[2] == L'h' || string[2] == L'H') &&
+        is_number(string[3]) &&
+        is_number(string[4]) &&
+        (string[5] == L'm' || string[5] == L'M'))
+        return 1;
+
+    return 0;
 }
 
 int time_to_string(wchar_t *string, const int string_length, const Hours time)
