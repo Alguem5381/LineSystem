c1edab4eff36a86a0ecbc0aeca4eeaf496c79df3
Rodrigo Cabral <rodrigocabral0529@gmail.com>
2025-11-25 22:53:05 -0400

    feat(Object): implement bus line management functions and enhance object deletion

--- DoubleList/object.c
+++ DoubleList/object.c
@@ -32,31 +32,128 @@ int deleteObject(Object *obj){
 
     while (curr)
     {
-        DoubleList *DL = (DoubleList*)(curr->info);
-        deleteLine(DL);
-
-        prev = curr;
+        BusLine *DL = ((BusLine*)curr->info);
+        printf("%s\n", DL->name);
         curr = curr->next;
-        free(prev);
     }
+    destroy(obj->SLL);
 
-    obj->SLL = NULL;
     return 1;
 }
 
-int deleteLine(DoubleList *dl){
+int deleteLine(DoubleList *dl) {
     if (!dl || !dl->head) return 0;
-    
-    Node *curr = dl->head;
-    Node *prev;
 
-    while (curr)
-    {
-        prev = curr;
-        curr = curr->next;
-        free(prev);
-    }
+    Node *curr = dl->head;
+    Node *next;
+
+    do {
+        next = curr->next;
+        
+        free(curr);
+        curr = next;
+    } while (curr != dl->head);
 
     dl->head = NULL;
+    dl->size = 0;
+
     return 1;
+}
+
+int removeByName(Object *obj, char *name) {
+    if (!obj || !obj->SLL || !obj->SLL->head) return 0;
+
+    SLL_Node *curr = obj->SLL->head;
+    SLL_Node *prev = NULL;
+
+    while (curr) {
+        BusLine *line = (BusLine*)curr->info;
+
+        if (line && !strcmp(line->name, name)) {
+            deleteLine(line->list);
+
+            if (prev == NULL) {
+                obj->SLL->head = curr->next;
+            } else {
+                prev->next = curr->next;
+            }
+
+            free(line);
+            free(curr);
+
+            return 1;
+        }
+
+        prev = curr;
+        curr = curr->next;
+    }
+
+    return 0;
+}
+
+
+
+int insertBusLine(Object *obj, char *name, char *enterprise)
+{
+    BusLine *bus = malloc(sizeof(BusLine));
+    if(!bus)
+        return 0;
+    DoubleList *DL = malloc(sizeof(DoubleList));
+    if(!DL)
+        return 0;
+    create(DL);
+    if(hasBusLine(obj,name))
+        return 0;
+    strcpy(bus->enterprise, enterprise);
+    strcpy(bus->name, name);
+    bus->list = DL;
+    if(!initInsert(obj->SLL, bus)){
+        free(bus);
+        return 0;
+    }
+    return 1;
+}
+
+int insertBusStop(Object *obj, char *lineName, int id, char *name)
+{
+    if (!obj || !obj->SLL) return 0;
+
+    BusStop *stop = malloc(sizeof(BusStop));
+    if (!stop) return 0;
+
+    stop->id = id;
+    strcpy(stop->nome, name);
+
+    SLL_Node *curr = obj->SLL->head;
+
+    while (curr) {
+        BusLine *line = (BusLine*) curr->info;
+
+        if (line && !strcmp(line->name, lineName)) {
+
+            if (!add(line->list, stop)) {
+                free(stop);
+                return 0;
+            }
+
+            return 1;
+        }
+
+        curr = curr->next;
+    }
+
+    free(stop);
+    return 0;
+}
+
+
+int hasBusLine(Object *obj, char *name){
+    SLL_Node *curr = obj->SLL->head;
+    while (curr)
+    {
+        if (!strcmp((((BusLine*)(curr->info))->name),name ))
+            return 1;
+        curr = curr->next;
+    }
+    return 0;
 }
--- DoubleList/object.h
+++ DoubleList/object.h
@@ -14,8 +14,12 @@ int defineObject(Object *object);
 int loadData(Object *object);
 int saveObject(Object *object);
 
-//WIP
 int deleteObject(Object *object);
 int deleteLine(DoubleList *dl);
+int removeByName(Object *obj, char *name);
+int insertBusStop(Object *obj, char *lineName, int id, char *name);
+int insertBusLine(Object *obj, char *name, char *enterprise);
+int hasBusLine(Object *obj, char *name);
+int removeStop(Object *obj, char *lineName, char *stopName);
 
 #endif
